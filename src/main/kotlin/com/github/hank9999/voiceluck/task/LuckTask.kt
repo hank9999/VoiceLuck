1cc6a5d7034d8df0d9e057e079ec36c024bb97cb
hank9999 <hank9999@qq.com>
2022-09-13 12:01:15 +0800

    feat: use kmongo filter

--- src/main/kotlin/com/github/hank9999/voiceluck/task/LuckTask.kt
+++ src/main/kotlin/com/github/hank9999/voiceluck/task/LuckTask.kt
@@ -2,10 +2,12 @@ package com.github.hank9999.voiceluck.task
 
 import com.github.hank9999.kook.utils.NamedThreadFactory
 import com.github.hank9999.voiceluck.VoiceLuck.db
+import com.github.hank9999.voiceluck.database.types.Luck
 import com.github.hank9999.voiceluck.handler.LuckHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
+import org.litote.kmongo.eq
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.*
@@ -15,8 +17,8 @@ class LuckTask : TimerTask() {
     override fun run() {
         coroutineScope.launch {
             try {
-                db.lucks.find().toFlow().collect {
-                    if (it.time - System.currentTimeMillis() <= 60 * 1000 && !it.isOpen) {
+                db.lucks.find(Luck::isOpen eq false).toFlow().collect {
+                    if (it.time - System.currentTimeMillis() <= 60 * 1000) {
                         LuckHandler.addLuckItem(it)
                     }
                 }
